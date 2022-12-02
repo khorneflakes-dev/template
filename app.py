aaca5d949a21088f0e4b8e3dea8a6c06b4971009
khorne flakes <khorneflakesdev@gmail.com>
2022-12-01 23:39:17 -0400

    a

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from sqlalchemy import create_engine
-
+import math
 
 # SQLAlchemy connectable
 engine = create_engine('mysql+pymysql://root:projectyelp2022@34.176.218.33/projectyelp')
 
-
 app = Dash(__name__)
-
 
 app.title = 'YELP'
 server = app.server
@@ -18,58 +16,243 @@ server = app.server
 
 
 app.layout = html.Div([
-    
     html.Div(dcc.RadioItems(id='aux', value='', className='aux')),
-    
     
     html.Div([
         
         html.Div([
+            html.Div([
+                html.Div([
+                    html.P('Welcome', id='welcome', className='welcome-title')
+                ],className='header-row1'),
+                html.Div([
+                    html.P('Discover new flavors', className='welcome-description'),
+                    html.Div([
+                        html.P(id='options'), #dropdown pendiente
+                        html.Img(src='') #svg de flecha pendiente
+                    ], className='options')
+                ],className='header-row2'),
+            ], className='header'),
             
-            dcc.Textarea(id='textarea2', className='user', placeholder='enter id user'),
+            html.Div(id='cards'
+                # html.Div([
+                #     html.P(id='restaurant-name', className='restaurant-name'),
+                #     html.Div([
+                #         html.Div([
+                            
+                #         ],className='atention-days'),
+                #         html.Div([
+                            
+                #         ], className='atention-hours')
+                #     ], className='atention'),
+                #     html.P(id='address', className='address'),
+                #     html.P(id='city-state'),
+                #     html.Div([
+                #         html.Div([
+                            
+                #         ], className='stars'),
+                #         html.P(id='n-reviews')
+                #     ], className='rating')
+                # ], className='card')
+            , className='cards')
+        ], className='col1'),
+        
+        html.Div([
+            html.Div([
+                dcc.Input(id='enter-id', className='enter-id', placeholder='enter id user'),
+                html.Button('recommend me', id='login', n_clicks=0, className='login'),
+            ], className='login-container'),
             
-            html.Button('login', id='textarea-state-example-button', n_clicks=0, className='login'),
-            
-        ], className='login-area'),
+            html.Div([
+                html.Iframe(id='iframe-map',src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBKqlE-X4gVVz0YNXpsJcMuaFEfqhkHIio&q=36.151387,-86.796603',
+                            style={'width':"1500", 'height':"1080", 'style':"border:0", 'loading':"lazy", 'referrerpolicy':"no-referrer-when-downgrade"}, className='iframe')   
+            ], className='map'),
+        ],className='col2'),
         
-        html.Div(id='day', className='day')
-        
-    ], className='row1-1'),
-        
-    html.Div([
-        html.Div('Welcome',id='textarea-state-example-output', className=''),
-    ], className='row1-2')
-       
+    ], className='users-container'),  
     
+    html.Div(id='demo')
 ], className = 'main-container')
 
 
 
 # funcion para validar el id_user
 @app.callback(
-    Output('textarea-state-example-output', 'children'),
-    Input('textarea-state-example-button', 'n_clicks'),
-    State('textarea2', 'value')
+    Output('welcome', 'children'),
+    Input('login', 'n_clicks'),
+    State('enter-id', 'value')
 )
 def update_output(n_clicks, value):
     if n_clicks > 0 and value != None:
-        return f'Welcome, {value}'
+        name = engine.connect().execute(f'select name from user_names where id_user = {value};').fetchall()[0][0]
+        return f'Welcome, {name}'
     else:
         return 'Welcome'
 
 
-
-# funcion para retornar el dia y la hora para la ciudad de argentina
+# funcion para crear las cards de los restaurantes recomendados
 @app.callback(
-    Output('day', 'children'),
-    [Input('aux', 'value')]
+    Output('cards', 'children'),
+    Input('login', 'n_clicks'),
+    State('enter-id', 'value')    
 )
-def actual_day(value):
-    from datetime import datetime
-    import pytz
-    dt = datetime.now(pytz.timezone('America/Buenos_Aires'))
-    return '{}, {}'.format(dt.strftime('%A'),dt.strftime("%H:%M"))
+def card(n_clicks, value):
+    engine = create_engine('mysql+pymysql://root:projectyelp2022@34.176.218.33/projectyelp')
+    if n_clicks == 0 and value == None:
+        recomendacion2 = pd.read_sql('select b.name, b.address, b.latitude, b.longitude, cs.city, cs.state, b.stars, b.review_count, h.Monday, h.Tuesday, h.Wednesday, h.Thursday, h.Friday, h.Saturday, h.Sunday from business b left join business_city_state cs on b.city_state_id = cs.city_state_id left join business_hours h on b.hours_id = h.hours_id order by b.review_count desc limit 9;',
+                                    con=engine)
+        
+        def stars(value):
+            decimal, entero = math.modf(value)
+            lista = ''
+            for i in list(range(int(entero))):
+                lista += 'A'
+            if decimal != 0:
+                lista += 'B'
 
+            for i in list(range(5)):
+                lista += 'C'
+
+            return lista[:5]
+
+        recomendacion2['estrellas'] = recomendacion2['stars'].apply(lambda x: stars(x))
+     
+        
+        demoval2 = html.Div([
+
+            html.Div([
+                html.P(recomendacion2.iloc[i,:]['name'], className='restaurant-name'),
+                html.Div([
+                    html.Div([
+                        html.P('Monday:'),
+                        html.P('Tuesday:'),
+                        html.P('Wednesday:'),
+                        html.P('Thursday:'),
+                        html.P('Friday:'),
+                        html.P('Saturday:'),
+                        html.P('Sunday:')
+                    ], className='atention-days'),
+                    html.Div([
+                        html.P(recomendacion2.iloc[i, :]['Monday']),
+                        html.P(recomendacion2.iloc[i, :]['Tuesday']),
+                        html.P(recomendacion2.iloc[i, :]['Wednesday']),
+                        html.P(recomendacion2.iloc[i, :]['Thursday']),
+                        html.P(recomendacion2.iloc[i, :]['Friday']),
+                        html.P(recomendacion2.iloc[i, :]['Saturday']),
+                        html.P(recomendacion2.iloc[i, :]['Sunday'])
+                    ], className='atention-hours')
+                ], className='atention'),
+                html.P(recomendacion2.iloc[i,:]['address'], className='address'),
+                html.P(recomendacion2.iloc[i,:]['city']+', '+recomendacion2.iloc[i,:]['state'], className='city-state'),
+                html.Div([
+                    html.Div([
+                        
+                        html.Img(src=f'./assets/star{j}.png', className='star1')
+                        for j in recomendacion2.iloc[i, :]['estrellas']
+                        
+                    ], className='stars'),
+                    
+                    html.P(str(recomendacion2.iloc[i, :]['review_count']) + ' reviews', className='review-count'),
+                ], className='rating')
+            ], className='card')
+
+
+            for i in list(range(len(recomendacion2)))
+        ], className='cards2')
+        engine.dispose()
+        return demoval2
+    
+    elif n_clicks > 0 and value != None:
+            
+        id_user = value
+        main_cat = pd.read_parquet('main_cat.parquet.gzip')
+
+        user_review = pd.read_sql(
+            f"select id_business, stars from reviews where id_user = {id_user};",
+            con=engine, index_col='id_business')
+        user_preferences = pd.merge(user_review, main_cat, left_index=True, right_index=True)
+        user_stars = user_preferences['stars'].copy()
+        user_preferences.drop(columns='stars', inplace=True)
+        user_stars.shape, user_preferences.transpose().shape
+        user_perfil = user_preferences.transpose().dot(user_stars)
+        recomendacion = (main_cat * user_perfil).sum(axis=1)/(user_perfil.sum())
+        recomendacion = recomendacion.sort_values(ascending=False)
+
+        tupla_consulta = tuple(recomendacion.keys()[:30])
+        recomendacion1 = pd.read_sql(f"select b.name, b.address, b.latitude, b.longitude, cs.city, cs.state, b.stars, b.review_count, h.Monday, h.Tuesday, h.Wednesday, h.Thursday, h.Friday, h.Saturday, h.Sunday from business b left join business_city_state cs on b.city_state_id = cs.city_state_id left join business_hours h on b.hours_id = h.hours_id where b.business_id in {tupla_consulta} order by b.review_count desc limit 9;",
+                                    con=engine)
+        
+        def stars(value):
+            decimal, entero = math.modf(value)
+            lista = ''
+            for i in list(range(int(entero))):
+                lista += 'A'
+            if decimal != 0:
+                lista += 'B'
+
+            for i in list(range(5)):
+                lista += 'C'
+
+            return lista[:5]
+
+        recomendacion1['estrellas'] = recomendacion1['stars'].apply(lambda x: stars(x))
+        
+        # recomendacion1.to_csv('recomendacion1.csv', index=False)
+        # recomendacion1 = pd.read_csv('./recomendacion1.csv')
+        
+        
+        demoval = html.Div([
+
+            html.Div([
+                html.P(recomendacion1.iloc[i,:]['name'], className='restaurant-name'),
+                html.Div([
+                    html.Div([
+                        html.P('Monday:'),
+                        html.P('Tuesday:'),
+                        html.P('Wednesday:'),
+                        html.P('Thursday:'),
+                        html.P('Friday:'),
+                        html.P('Saturday:'),
+                        html.P('Sunday:')
+                    ], className='atention-days'),
+                    html.Div([
+                        html.P(recomendacion1.iloc[i, :]['Monday']),
+                        html.P(recomendacion1.iloc[i, :]['Tuesday']),
+                        html.P(recomendacion1.iloc[i, :]['Wednesday']),
+                        html.P(recomendacion1.iloc[i, :]['Thursday']),
+                        html.P(recomendacion1.iloc[i, :]['Friday']),
+                        html.P(recomendacion1.iloc[i, :]['Saturday']),
+                        html.P(recomendacion1.iloc[i, :]['Sunday'])
+                    ], className='atention-hours')
+                ], className='atention'),
+                html.Div([
+                    html.P(recomendacion1.iloc[i,:]['address'], className='address'),
+                    html.P(recomendacion1.iloc[i,:]['city']+', '+recomendacion1.iloc[i,:]['state'], className='city-state'),
+                    html.Button('ver mapa', id='map', n_clicks=0, className='button-map'),
+                ], className='direccion'),
+                html.Div([
+                    html.Div([
+                        
+                        html.Img(src=f'./assets/star{j}.png', className='star1')
+                        for j in recomendacion1.iloc[i, :]['estrellas']
+                        
+                    ], className='stars'),
+                    html.P(str(recomendacion1.iloc[i, :]['review_count']) + ' reviews', className='review-count'),
+                    
+                ], className='rating')
+            ], className='card')
+
+
+            for i in list(range(len(recomendacion1)))
+        ], className='cards2')
+        engine.dispose()
+        
+        return demoval
+
+# @app.callback(
+#     Output('iframe-map', 'children'),
+#     Input('map', 'n_clicks')
+# )
 
 
 if __name__ == '__main__':
--- maps.py
+++ maps.py
@@ -1,15 +1,18 @@
-main_cat = pd.read_parquet('main_cat.parquet.gzip')
-engine = create_engine('mysql+pymysql://root:projectyelp2022@34.176.218.33/projectyelp')
-user_review = pd.read_sql(
-    "select id_business, stars from reviews where id_user = 200;",
-    con=engine, index_col='id_business'
-)
-user_preferences = pd.merge(user_review, main_cat, left_index=True, right_index=True )
-user_stars = user_preferences['stars'].copy()
-user_preferences.drop(columns='stars', inplace=True)
-user_stars.shape, user_preferences.transpose().shape
-user_perfil = user_preferences.transpose().dot(user_stars)
-recomendacion = (main_cat * user_perfil).sum(axis=1)/(user_perfil.sum())
-recomendacion = recomendacion.sort_values(ascending=False)
-recom_final = df_business.loc[df_business['business_id'].isin(recomendacion.keys())]
-recom_final[recom_final['is_open']==1].sort_values('review_count', ascending=False)
+import gmaps
+import gmaps.datasets
+gmaps.configure(api_key='AIzaSyAP8lAVlpx6dMZbMgDlegUlHcu2vGOa6vk')
+
+marker_locations = [
+(51.216671, 5.0833302),
+(51.333328, 4.25)
+]
+ 
+ 
+fig = gmaps.figure()
+markers = gmaps.marker_layer(marker_locations)
+fig.add_layer(markers)
+
+
+from ipywidgets.embed import embed_minimal_html
+fig = gmaps.figure()
+embed_minimal_html('index.html', views=[fig])
